a2a4c94298ac030548f4a6bc1dc120eb853cef3d
muhammedshibilin <nkshibili17@gmail.com>
2025-06-04 15:50:25 +0530

    rebuilded

--- dist/routes/product.route.js
+++
@@ -1,6 +0,0 @@
-import { Router } from 'express';
-import * as productController from '../controllers/product.controller';
-const router = Router();
-router.get('/', productController.getAllProducts);
-router.post('/', productController.createProduct);
-export default router;
